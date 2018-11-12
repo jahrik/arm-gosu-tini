d75d210f987463196134a6ff7793e835beac8933
Wes Gill <wgill@kickbackpoints.com>
2018-11-11 16:34:39 -0800

    Initial commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+# If GOSU_CHOWN environment variable set, recursively chown all specified directories 
+# to match the user:group set in GOSU_USER environment variable.
+if [ -n "$GOSU_CHOWN" ]; then
+    for DIR in $GOSU_CHOWN
+    do
+        chown -R $GOSU_USER $DIR
+    done
+fi  
+
+# If GOSU_USER environment variable set to something other than 0:0 (root:root),
+# become user:group set within and exec command passed in args
+if [ "$GOSU_USER" != "0:0" ]; then
+	  set -- gosu $GOSU_USER tini -- "$@"
+else
+    set -- tini -- "$@"
+fi
+
+exec "$@"
